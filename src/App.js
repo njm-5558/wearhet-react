4bd869c91e0cc286fdd3f886fbbd8481ea0a45b4
njm-5558 <najmebabai@gmail.com>
2022-05-15 20:24:22 +0430

    adding a ode source

--- src/App.js
+++ src/App.js
@@ -4,26 +4,31 @@ import Weather from "./Weather";
 
 function App() {
   return (
-        <div className="container">
-          <div className="row">
-            <div className="col-9">
-              <h1 className="card-title">Tehran</h1>
-              <h2 className="card-subtitle mb-2 text-muted" id="date">
-                Sunday 1:19{" "}
-              </h2>
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-9">
+            <h1 className="card-title">Tehran</h1>
+            <h2 className="card-subtitle mb-2 text-muted" id="date">
+              Sunday 1:19{" "}
+            </h2>
 
-              <h3 id="description"> ☁ Clear</h3>
-            </div>
-            <div className="col-3">
-              <div className="feature">
-                <div className="humidity"> humidity: 75% </div>
-                <div className="wind"> wind: 4 km/h </div>
-              </div>
-            </div>
-            <Weather />
+            <h3 id="description"> ☁ Clear</h3>
           </div>
+          <div className="col-3">
+            <div className="feature">
+              <div className="humidity"> humidity: 75% </div>
+              <div className="wind"> wind: 4 km/h </div>
+            </div>
+          </div>
+          <Weather />
         </div>
-     
+      </div>
+      <div className="GitHub-link">
+        <a href="https://github.com/njm-5558/wearhet-react">Open-source code</a>
+        <span> ,by Najme Babai, SheCodes student</span>
+      </div>
+    </div>
   );
 }
 
