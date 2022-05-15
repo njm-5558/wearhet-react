c3fef32bc86e466e2b0909d6f2f61884fbdd029c
njm-5558 <najmebabai@gmail.com>
2022-05-15 18:05:03 +0430

    creating a  weather app with a search form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-9">
+              <h1 className="card-title">Tehran</h1>
+              <h2 className="card-subtitle mb-2 text-muted" id="date">
+                Sunday 1:19{" "}
+              </h2>
+
+              <h3 id="description"> ☁ Clear</h3>
+            </div>
+            <div className="col-3">
+              <div className="feature">
+                <div className="humidity"> humidity: 75% </div>
+                <div className="wind"> wind: 4 km/h </div>
+              </div>
+            </div>
+            <Weather />
+          </div>
+        </div>
+     
   );
 }
 
